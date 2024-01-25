ebb65a92cf6e56b49172733c06eb6accbe67cb55
benammar87 <ben_ammar_m@live.fr>
2024-01-25 16:44:51 +0100

    update

---
+++ Pre_Bootcamp/My Example/BenAmmar_Mohamed_JSPredict.js
@@ -0,0 +1,27 @@
+// predict 1
+
+function myBirthYearFunc(){
+        console.log("I was born in " + 1980);// i was born in 1980
+
+    }
+    
+
+//  predict 2
+
+function myBirthYearFunc(1980){
+        console.log("I was born in " + 1980); // i was born in 1980
+    }
+    
+
+// predict 3
+
+function add(num1, num2){
+        console.log("Summing Numbers!");// 
+        console.log("num1 is: " + num1);
+        console.log("num2 is: " + num2);
+        var sum = num1 + num2;
+        console.log(sum);
+    }
+    
+    
+    
