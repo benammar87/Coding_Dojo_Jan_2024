5604b45a23d4747d719e2eabc9fadb41a660296d
benammar87 <ben_ammar_m@live.fr>
2024-02-29 14:11:46 +0100

    users_and_books

--- Python/Week 2/Day 2/Practice/Books_SQL/boooks.sql
+++ Python/Week 2/Day 2/Practice/Books_SQL/boooks.sql
@@ -9,3 +9,17 @@ WHERE books.id = 3 ;
 
 delete from books_has_users
 where(user_id=2)and(book_id=3);
+
+SELECT * FROM users
+LEFT JOIN books_has_users
+ON users.id = books_has_users.user_id
+LEFT JOIN books
+ON books_has_users.book_id = books.id
+WHERE users.id = 3 ;
+
+SELECT * FROM users
+LEFT JOIN books_has_users
+ON users.id = books_has_users.user_id
+LEFT JOIN books
+ON books_has_users.book_id = books.id
+WHERE books.id = 5 ;
