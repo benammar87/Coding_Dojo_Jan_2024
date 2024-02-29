1c960813eb411743e9b1bc513de085f1d802d34a
benammar87 <ben_ammar_m@live.fr>
2024-02-29 14:09:53 +0100

    users_and_books

---
+++ Python/Week 2/Day 2/Practice/Books_SQL/boooks.sql
@@ -0,0 +1,11 @@
+SELECT * FROM books_schema.books_has_users;
+--  Retrieve all the users who favorited the 3rd book
+SELECT * FROM users
+LEFT JOIN books_has_users
+ON users.id = books_has_users.user_id
+LEFT JOIN books
+ON books_has_users.book_id = books.id
+WHERE books.id = 3 ;
+
+delete from books_has_users
+where(user_id=2)and(book_id=3);
