7790ed0745776694b3b835a36e27f931f526cdc5
benammar87 <ben_ammar_m@live.fr>
2024-03-01 15:46:44 +0100

    users_CR

--- Python/Week 2/Day 3/Practice/Users_CR/user.py
+++ Python/Week 2/Day 3/Practice/Users_CR/user.py
@@ -3,7 +3,6 @@ from mysqlconnection import connectToMySQL
 
 DATABASE = "users"
 
-
 # === C R U D ===
 
 # model the class after the friend table from our database
@@ -33,21 +32,12 @@ class User:
             return users_instances
         
         return []
-    
-    @classmethod
-    def get_by_id(cls, user_id):
-        query = "SELECT * FROM users WHERE id = %(user_id)s;"
-        data = {'user_id': user_id}
-        result = connectToMySQL(DATABASE).query_db(query, data)
-        if result:
-            return cls(result[0])  # Assuming the query returns only one user
-        else:
-            return None
         
     #* =========== CREATE ===========
 
     @classmethod
     def save(cls, data):
+
         query = """
                 INSERT INTO users (first_name, last_name, email) 
                 VALUES (%(first_name)s, %(last_name)s, %(email)s);
@@ -55,29 +45,3 @@ class User:
         result = connectToMySQL(DATABASE).query_db(query, data)
         
         return result
-    
-    #* =========== UPDATE ===========
-
-    @classmethod
-    def update(cls, user_id, first_name, last_name, email):
-        query = """
-                UPDATE users
-                SET first_name = %(first_name)s, last_name = %(last_name)s, email = %(email)s
-                WHERE id = %(id)s;
-                """
-        data = {
-            'id': user_id,
-            'first_name': first_name,
-            'last_name': last_name,
-            'email': email
-        }
-        connectToMySQL(DATABASE).query_db(query, data)
-    
-    
-    #* =========== DELETE ===========
-
-    @classmethod
-    def delete(cls, id):
-        query = "DELETE FROM users WHERE id = %(id)s;"
-        connectToMySQL(DATABASE).query_db(query, {'id': id})
-
