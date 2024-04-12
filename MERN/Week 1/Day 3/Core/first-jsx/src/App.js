a4b3dddbeb3f6d9cebf73011f7c0ab7f644e2b6a
mohamed ben ammar <ben_ammar_m@live.fr>
2024-04-12 14:07:52 +0100

    first react with JSX

---
+++ MERN/Week 1/Day 3/Core/first-jsx/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo !</h1><br />
+      <h3>Things I need to do </h3>
+      <table>
+        <li> Learn React</li>
+        <li> climb Mt.Everest</li>
+        <li> Run a Marathon</li>
+        <li> Feed the dogs</li>
+      </table>
+    </div>
+  );
+}
+
+export default App;
