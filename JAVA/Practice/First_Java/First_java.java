06d44a98e32d2b948cea201f465685bea2d9d1c1
mohamed ben ammar <ben_ammar_m@live.fr>
2024-07-12 19:43:30 +0100

    First_Java

---
+++ JAVA/Practice/First_Java/First_java.java
@@ -0,0 +1,7 @@
+public class First_java {
+    public static void main(String[] args){
+        System.out.println("My name is Mohamed");
+        System.out.println("I am 37 years old");
+        System.out.println("My hometown is Bardo");
+    }
+}
