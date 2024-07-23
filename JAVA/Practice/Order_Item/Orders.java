83c3510c773b35031052c897293d30b09238f136
mohamed ben ammar <ben_ammar_m@live.fr>
2024-07-23 18:30:47 +0100

    Order_Item
    git commit -m Order_Item

---
+++ JAVA/Practice/Order_Item/Orders.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+/**
+ * Orders
+ */
+public class Orders {
+
+    public String name ;
+    public double total ;
+    public boolean ready ;
+    ArrayList<Item> items = new ArrayList<Item>();
+}
---
+++ JAVA/Practice/Order_Item/TestOrders.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+public class TestOrders {
+    public static void main(String[] args) {
+    
+        // Menu items
+        Item item1 = new Item();
+        Item item2 = new Item();
+        Item item3 = new Item();
+        Item item4 = new Item();
+        item1.name = "latte";
+        item1.price = 5.5 ;
+        item2.name = "cappucino" ;
+        item2.price = 7.2 ;
+        item3.name ="mocha";
+        item3.price = 8.5 ;
+        item4.name = "drop coffe";
+        item4.price= 10 ;
+
+    
+        // Order variables -- order1, order2 etc.
+        Orders order1 = new Orders();
+        Orders order2 = new Orders();
+        Orders order3 = new Orders();
+        Orders order4 = new Orders();
+        order1.name = "Cindhury";
+        order2.name = "Noah";
+        order3.name ="Jimmy";
+        order4.name="Sam";
+        order1.total = item1.price + item2.price ;
+        order1.ready = true ;
+
+        order2.items.add(item2);
+        order2.total +=item2.price;
+        order3.items.add(item3);
+        order3.total += item3.price;
+        order3.ready = true ;
+        
+    
+        // Application Simulations
+        // Use this example code to test various orders' updates
+        System.out.printf("Name: %s\n", order1.name);
+        System.out.printf("Total: %s\n", order1.total);
+        System.out.printf("Ready: %s\n", order1.ready);
+        System.out.printf("Name: %s\n", order2.name);
+        System.out.printf("Total: %s\n", order2.total);
+        System.out.printf("Ready: %s\n", order2.ready);
+        System.out.printf("Name: %s\n", order3.name);
+        System.out.printf("Total: %s\n", order3.total);
+        System.out.printf("Ready: %s\n", order3.ready);
+    }
+}
+
